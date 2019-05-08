8eabed9d3bbd7ae4e32f02d97709e7aac96b720a
Ilari <ilari.m97@gmail.com>
2019-05-08 12:14:22 +0300

    Pisteet näkyvissä

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        count = 0;
+        countText.text = "Pisteet: " + count.ToString();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -33,6 +36,12 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Pisteet: " + count.ToString();
+            if (count > 4)
+            {
+                winText.text = "VOITIT PELIN";
+            }
         }
     }
 }
